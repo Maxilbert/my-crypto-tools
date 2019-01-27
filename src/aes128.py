1cafce16b0de508733f5f93092142f9e1f79562b
Maxilbert <yl768@njit.edu>
2019-01-27 15:23:12 -0500

    aes128 decryption

--- src/aes128.py
+++ src/aes128.py
@@ -62,6 +62,26 @@ EXP = \
         0x66, 0xb2, 0x76, 0x60, 0xda, 0xc5, 0xf3, 0xf6, 0xaa, 0xcd, 0x9a, 0xa0, 0x75, 0x54, 0x0e, 0x01
     ]
 
+INV_SBOX = \
+[
+    0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+    0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+    0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+    0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+    0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+    0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+    0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+    0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+    0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+    0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+    0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+    0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+    0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+    0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+    0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+]
+
 
 def aes128_enc(msg, key):
     state = \
@@ -90,6 +110,112 @@ def aes128_enc(msg, key):
     state = sub_state(state)
     state = shift_rows(state)
     state = add_round_key(state, expanded_key[40:44])
+    return [state[0][0], state[1][0], state[2][0], state[3][0],
+            state[0][1], state[1][1], state[2][1], state[3][1],
+            state[0][2], state[1][2], state[2][2], state[3][2],
+            state[0][3], state[1][3], state[2][3], state[3][3]]
+
+
+def sub_state(state):
+    for i in range(len(state)):
+        for j in range(len(state[i])):
+            state[i][j] = SBOX[state[i][j]]
+    return state
+
+
+def shift_rows(state):
+    s_0 = state[0]
+    s_1 = [state[1][1], state[1][2], state[1][3], state[1][0]]
+    s_2 = [state[2][2], state[2][3], state[2][0], state[2][1]]
+    s_3 = [state[3][3], state[3][0], state[3][1], state[3][2]]
+    return [s_0, s_1, s_2, s_3]
+
+
+def mix_columns(state):
+    cpy = copy.deepcopy(state)
+    for i in range(0, 4):
+        state[0][i] = gal_mul(cpy[0][i], 2) ^ cpy[3][i] ^ cpy[2][i] ^ gal_mul(cpy[1][i], 3)
+        state[1][i] = gal_mul(cpy[1][i], 2) ^ cpy[0][i] ^ cpy[3][i] ^ gal_mul(cpy[2][i], 3)
+        state[2][i] = gal_mul(cpy[2][i], 2) ^ cpy[1][i] ^ cpy[0][i] ^ gal_mul(cpy[3][i], 3)
+        state[3][i] = gal_mul(cpy[3][i], 2) ^ cpy[2][i] ^ cpy[1][i] ^ gal_mul(cpy[0][i], 3)
+    return state
+
+
+def gal_mul(x, y):
+    if (x == 0) or (y == 0):
+        return 0
+    x_log = LOG[x]
+    y_log = LOG[y]
+    exp_value = (x_log + y_log) % 255
+    return EXP[exp_value]
+
+
+def aes128_dec(cipher, key):
+    state = \
+        [
+            [cipher[0], cipher[4], cipher[8], cipher[12]],
+            [cipher[1], cipher[5], cipher[9], cipher[13]],
+            [cipher[2], cipher[6], cipher[10], cipher[14]],
+            [cipher[3], cipher[7], cipher[11], cipher[15]]
+        ]
+    expanded_key = expand_key(key)
+    # Round-0
+    print("The 0-th round")
+    print(state)
+    state = add_round_key(state, expanded_key[40:44])
+    # Round-1 to Round-9add_round_key
+    for r in reversed(range(1, 10)):
+        print("The %d-th round" % (10 - r))
+        print(state)
+        state = inv_shift_rows(state)
+        print(state)
+        state = inv_sub_state(state)
+        print(state)
+        add_round_key(state, expanded_key[4*r:4*r+4])
+        print(state)
+        state = inv_mix_columns(state)
+        print(state)
+    # Round-10
+    print("The 10-th round")
+    print(state)
+    state = inv_shift_rows(state)
+    state = inv_sub_state(state)
+    state = add_round_key(state, expanded_key[0:4])
+    return [state[0][0], state[1][0], state[2][0], state[3][0],
+            state[0][1], state[1][1], state[2][1], state[3][1],
+            state[0][2], state[1][2], state[2][2], state[3][2],
+            state[0][3], state[1][3], state[2][3], state[3][3]]
+
+
+def inv_sub_state(state):
+    for i in range(len(state)):
+        for j in range(len(state[i])):
+            state[i][j] = INV_SBOX[state[i][j]]
+    return state
+
+
+def inv_shift_rows(state):
+    s_0 = state[0]
+    s_1 = [state[1][3], state[1][0], state[1][1], state[1][2]]
+    s_2 = [state[2][2], state[2][3], state[2][0], state[2][1]]
+    s_3 = [state[3][1], state[3][2], state[3][3], state[3][0]]
+    return [s_0, s_1, s_2, s_3]
+
+
+def inv_mix_columns(state):
+    cpy = copy.deepcopy(state)
+    for i in range(0, 4):
+        state[0][i] = gal_mul(cpy[0][i], 14) ^ gal_mul(cpy[3][i], 9) ^ gal_mul(cpy[2][i], 13) ^ gal_mul(cpy[1][i], 11)
+        state[1][i] = gal_mul(cpy[1][i], 14) ^ gal_mul(cpy[0][i], 9) ^ gal_mul(cpy[3][i], 13) ^ gal_mul(cpy[2][i], 11)
+        state[2][i] = gal_mul(cpy[2][i], 14) ^ gal_mul(cpy[1][i], 9) ^ gal_mul(cpy[0][i], 13) ^ gal_mul(cpy[3][i], 11)
+        state[3][i] = gal_mul(cpy[3][i], 14) ^ gal_mul(cpy[2][i], 9) ^ gal_mul(cpy[1][i], 13) ^ gal_mul(cpy[0][i], 11)
+    return state
+
+
+def add_round_key(state, round_key):
+    for i in range(0, 4):
+        for j in range(0, 4):
+            state[i][j] = state[i][j] ^ round_key[j][i]
     return state
 
 
@@ -119,51 +245,15 @@ def sub_word(w):
     return w
 
 
-def sub_state(state):
-    for i in range(len(state)):
-        for j in range(len(state[i])):
-            state[i][j] = SBOX[state[i][j]]
-    return state
-
-
-def shift_rows(state):
-    s_0 = state[0]
-    s_1 = [state[1][1], state[1][2], state[1][3], state[1][0]]
-    s_2 = [state[2][2], state[2][3], state[2][0], state[2][1]]
-    s_3 = [state[3][3], state[3][0], state[3][1], state[3][2]]
-    return [s_0, s_1, s_2, s_3]
-
-
-def mix_columns(state):
-    cpy = copy.deepcopy(state)
-    for i in range(0, 4):
-        state[0][i] = gal_mul(cpy[0][i], 2) ^ cpy[3][i] ^ cpy[2][i] ^ gal_mul(cpy[1][i], 3)
-        state[1][i] = gal_mul(cpy[1][i], 2) ^ cpy[0][i] ^ cpy[3][i] ^ gal_mul(cpy[2][i], 3)
-        state[2][i] = gal_mul(cpy[2][i], 2) ^ cpy[1][i] ^ cpy[0][i] ^ gal_mul(cpy[3][i], 3)
-        state[3][i] = gal_mul(cpy[3][i], 2) ^ cpy[2][i] ^ cpy[1][i] ^ gal_mul(cpy[0][i], 3)
-    return state
-
-
-def add_round_key(state, round_key):
-    for i in range(0, 4):
-        for j in range(0, 4):
-            state[i][j] = state[i][j] ^ round_key[j][i]
-    return state
-
-
-def gal_mul(x, y):
-    #if (x == 0) or (y == 0):
-    #    return 0
-    x_log = LOG[x]
-    y_log = LOG[y]
-    exp_value = (x_log + y_log) % 255
-    return EXP[exp_value]
-
 
 raw_key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
 plaintext = [0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a]
 
 print("Expanded key:")
 print(expand_key(raw_key))
-print("Ciphertext:")
-print(aes128_enc(plaintext, raw_key))
+print("Encryption:")
+ciphertext = aes128_enc(plaintext, raw_key)
+print(ciphertext)
+print("Decryption:")
+plaintext = aes128_dec(ciphertext, raw_key)
+print(plaintext)
